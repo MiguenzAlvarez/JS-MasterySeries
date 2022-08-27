f35ff5bf4f2e798714165e1a7238f2b8cdbc8947
MiguenzAlvarez <luly_mi@hotmail.com>
2022-08-27 17:11:10 -0300

    Added braces to showProperties function.

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ showProperties(movie);
 
 function showProperties(obj) {
   for (let key in obj) {
-    if (typeof obj[key] === 'string') console.log(key, obj[key]);
+    if (typeof obj[key] === 'string') {
+      console.log(key, obj[key]);
+    }
   }
 }
